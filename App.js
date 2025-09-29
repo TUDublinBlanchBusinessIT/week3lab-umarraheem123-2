a5ed2c03a508b8aa4f505eb729e483302e44bbd4
B00152481 Umar Raheem <B00152481@mytudublin.ie>
2025-09-29 16:23:59 +0100

    Step 1 completed

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import {View, Text, TextInput} from 'react-native';
 import React, {useState} from 'react';
-
+import Logo from './components/Logo'
 
 export default function App(){
   const [fullname, setFullname] = useState("Eoin");
+
   return(
-    <View> 
+    <View>
+      <Logo/>
       <Text>Hello, World {fullname}</Text>
-      <TextInput placeholder="enter your name" onChangeText={(value)=>setFullname(value)}></TextInput>
-    </View>
+      <TextInput placeholder="enter your name" onChangeText={setFullname}>
+      </View>
   )
-};
-
-
-
+}
